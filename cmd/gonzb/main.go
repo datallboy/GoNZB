b070e2a41c992e8601b1f71ea0bbaeb8a1798b42
Datallboy <github@datallboy.com>
2025-12-31 14:10:16 -0500

    Setup cobra comamnd line flags

--- cmd/gonzb/main.go
+++ cmd/gonzb/main.go
@@ -14,9 +14,57 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/spf13/cobra"
 )
 
-func main() {
+var (
+	nzbPath string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gonzb",
+	Short: "GONZB is a simple Usenet downloader",
+	Long:  `A lightweight, concurrent NNTP downloaer written in Go.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if nzbPath == "" {
+			fmt.Println("Error: --file or -f is required")
+			cmd.Help()
+			return
+		}
+
+		executeDownload()
+	},
+}
+
+func init() {
+	// Define flags
+	rootCmd.Flags().StringVarP(&nzbPath, "file", "f", "", "Path to the NZB file (required)")
+}
+
+func executeDownload() {
+	// 1. Create a channel for OS signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	// Setup Signal Handling for Graceful Shutdown
+	// We create a context that is cancelled when the user hits Ctrl+C
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		select {
+		case <-sigChan:
+			fmt.Println("\n\r[!] Interrupt received. Shutting down gracefully...")
+			cancel()
+
+		case <-ctx.Done():
+			// Context was cancelled normally (download finished), just exit
+			fmt.Print("\n\r Process finished successfully")
+			return
+		}
+	}()
+
 	// Load config
 	cfg, err := config.Load("config.yaml")
 	if err != nil {
@@ -44,30 +92,8 @@ func main() {
 	// Initialize the Downloader Service
 	svc := downloader.NewService(cfg, mgr)
 
-	// 1. Create a channel for OS signals
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-
-	// Setup Signal Handling for Graceful Shutdown
-	// We create a context that is cancelled when the user hits Ctrl+C
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	go func() {
-		select {
-		case <-sigChan:
-			fmt.Println("\n\r[!] Interrupt received. Shutting down gracefully...")
-			cancel()
-
-		case <-ctx.Done():
-			// Context was cancelled normally (download finished), just exit
-			fmt.Print("\n\r Process finished successfully")
-			return
-		}
-	}()
-
-	// TODO - read from cmd line argument
-	f, err := os.Open("test_file.nzb")
+	// Read NZB file from cmd line flag
+	f, err := os.Open(nzbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -86,5 +112,12 @@ func main() {
 		} else {
 			log.Fatalf("Download failed: %v", err)
 		}
+	}
+
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
 	}
 }
