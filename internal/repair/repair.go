c612791d2f99b97c3d062dac9cdda57141672a69
Datallboy <github@datallboy.com>
2026-01-06 14:50:01 -0500

    feat: add par2cmdline exec, repair, and verify logic feat: added completed dir logic after par2 checks

---
+++ internal/repair/repair.go
@@ -0,0 +1,11 @@
+package repair
+
+// Repairer defines the behavior for verifying and fixing downloads
+type Repairer interface {
+	// Verify checks if the file in the directory are healthy.
+	// Returns true if healthy, false if repair is needed.
+	Verify(path string) (bool, error)
+
+	// Repair attempts to fix the files using available parity volumes.
+	Repair(path string) error
+}
