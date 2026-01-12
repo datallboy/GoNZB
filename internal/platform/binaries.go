5bc6a52fe0cff5723495f68a6a7951042a7baebf
Datallboy <github@datallboy.com>
2026-01-12 15:53:43 -0500

    feat: base archive support

--- internal/platform/binaries.go
+++ internal/platform/binaries.go
@@ -10,11 +10,25 @@ var RequiredBinaries = []string{
 	"par2",
 }
 
+var OptionalExtractorBinaries = map[string]string{
+	"unrar": "RAR",
+	"unzip": "ZIP",
+	"7z":    "7-Zip",
+	"7za":   "7-Zip",
+}
+
 func ValidateDependencies() error {
 	for _, bin := range RequiredBinaries {
 		_, err := exec.LookPath(bin)
 		if err != nil {
 			return fmt.Errorf("required dependency: '%s' not found in PATH", bin)
+		}
+	}
+
+	// Check optional extractors
+	for bin, formatName := range OptionalExtractorBinaries {
+		if _, err := exec.LookPath(bin); err != nil {
+			fmt.Printf("Info: %s (%s) not found. %s extraction will be disabled.\n", bin, formatName, formatName)
 		}
 	}
 
