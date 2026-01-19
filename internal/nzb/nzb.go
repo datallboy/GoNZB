774ede98c3c1720070d0af068be0335c540a351c
Datallboy <github@datallboy.com>
2026-01-19 15:53:10 -0500

    fix: support password protected archives

--- internal/nzb/nzb.go
+++ internal/nzb/nzb.go
@@ -1,10 +1,19 @@
 package nzb
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"strings"
+)
 
 type Model struct {
 	XMLName xml.Name `xml:"nzb"`
+	Meta    []Meta   `xml:"head>meta"`
 	Files   []File   `xml:"file"`
+}
+
+type Meta struct {
+	Type    string `xml:"type,attr"`
+	Content string `xml:",chardata"`
 }
 
 type File struct {
@@ -19,4 +28,14 @@ type Segment struct {
 	Number    int      `xml:"number,attr"`
 	Bytes     int64    `xml:"bytes,attr"`
 	MessageID string   `xml:",chardata"`
+}
+
+// Helper method to grab the password from the slice of Meta tags
+func (n *Model) GetPassword() string {
+	for _, m := range n.Meta {
+		if m.Type == "password" {
+			return strings.TrimSpace(m.Content)
+		}
+	}
+	return ""
 }
