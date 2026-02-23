e50ab2201ca7ef40be1e708b2bba085d072a0b19
Datallboy <github@datallboy.com>
2026-02-23 09:38:38 -0500

    Add optional API key auth and redact apikey in request logs

--- internal/infra/config/config.go
+++ internal/infra/config/config.go
@@ -15,6 +15,7 @@ type Config struct {
 	Download DownloadConfig  `mapstructure:"download" yaml:"download"`
 	Log      LogConfig       `mapstructure:"log" yaml:"log"`
 	Store    StoreConfig     `mapstructure:"store" yaml:"store"`
+	API      APIConfig       `mapstructure:"api" yaml:"api"`
 
 	Port string `mapstructure:"port" yaml:"port"`
 }
@@ -53,6 +54,10 @@ type LogConfig struct {
 type StoreConfig struct {
 	SQLitePath string `mapstructure:"sqlite_path" yaml:"sqlite_path"`
 	BlobDir    string `mapstructure:"blob_dir" yaml:"blob_dir"`
+}
+
+type APIConfig struct {
+	Key string `mapstructure:"key" yaml:"key"`
 }
 
 func Load(path string) (*Config, error) {
